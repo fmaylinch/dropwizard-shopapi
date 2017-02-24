1f32592a251cfd8b9d8facde61604154855a2d2f
fmaylinch <ferranmaylinch@gmail.com>
2017-02-24 18:09:43 +0100

    Prepare dropwizard application

---
+++ src/main/java/com/codethen/shopapi/Product.java
@@ -0,0 +1,44 @@
+package com.codethen.shopapi;
+
+import java.util.Date;
+
+public class Product {
+
+	private String name;
+	private double price;
+	private boolean available;
+	private Date createdOn;
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+}
